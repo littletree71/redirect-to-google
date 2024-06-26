ada9d506365b30d0436cf2f34d22de00f630ae47
littletree71 <eric8271@gmail.com>
2024-06-26 19:34:03 +0800

    add block to bing

--- background.js
+++ background.js
@@ -3,8 +3,8 @@ chrome.webNavigation.onBeforeNavigate.addListener(details => {
   const url = new URL(details.url);
   if (url.hostname.includes("bing.com")) {
     const searchParams = url.searchParams.get("q");
-    const googleUrl = searchParams ? 
-      `https://www.google.com/search?q=${encodeURIComponent(searchParams)}` : 
+    const googleUrl = searchParams ?
+      `https://www.google.com/search?q=${encodeURIComponent(searchParams)}` :
       "https://www.google.com";
 
     console.log(`Redirecting to: ${googleUrl}`);
