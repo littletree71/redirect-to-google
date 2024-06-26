ce98299cdbf1e149c19d8edfe20decaf84ad7cd0
littletree71 <eric8271@gmail.com>
2024-06-26 19:01:05 +0800

    fix edge close bug when on tab

--- background.js
+++ background.js
@@ -1,17 +1,15 @@
 chrome.webNavigation.onBeforeNavigate.addListener(details => {
-    console.log(`URL about to navigate to: ${details.url}`);
-    const url = new URL(details.url);
-    if (url.hostname.includes("bing.com")) {
-      const searchParams = url.searchParams.get("q");
-      chrome.tabs.remove(details.tabId, () => {
-        if (searchParams) {
-          const googleUrl = `https://www.google.com/search?q=${encodeURIComponent(searchParams)}`;
-          console.log(`Redirecting to: ${googleUrl}`);
-          chrome.tabs.create({ url: googleUrl });
-        } else {
-          chrome.tabs.create({ url: "https://www.google.com" });
-        }
-      });
-    }
-  }, { url: [{ hostSuffix: "bing.com" }] });
-  
+  console.log(`URL about to navigate to: ${details.url}`);
+  const url = new URL(details.url);
+  if (url.hostname.includes("bing.com")) {
+    const searchParams = url.searchParams.get("q");
+    const googleUrl = searchParams ? 
+      `https://www.google.com/search?q=${encodeURIComponent(searchParams)}` : 
+      "https://www.google.com";
+
+    console.log(`Redirecting to: ${googleUrl}`);
+    chrome.tabs.create({ url: googleUrl }, () => {
+      chrome.tabs.remove(details.tabId);
+    });
+  }
+}, { url: [{ hostSuffix: "bing.com" }] });
